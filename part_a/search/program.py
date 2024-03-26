30c7e03f18f5a9ee36ad1d81aa91728411a8e3da
victoriahason <157922655+victoriahason@users.noreply.github.com>
2024-03-27 01:23:02 +1100

    help i want to cry

--- part_a/search/program.py
+++ part_a/search/program.py
@@ -5,6 +5,15 @@ from .core import PlayerColor, Coord, PlaceAction
 from .utils import render_board
 from queue import PriorityQueue
 
+def manhattan_distance(coord, targetcoord):
+    return (abs(targetcoord.r-coord.r)+ abs(targetcoord.c-coord.c))
+
+def relax(node1, node2):
+    if() ##do i create a class node which has a field coord and a field value????????????
+
+def get_adjacent_available_coords(coord):
+    return [ Coord((coord.r)+1, coord.y), Coord((coord.r)-1, coord.y), Coord(coord.r, (coord.y)+1), Coord(coord.r+1, (coord.y)-1)]
+
 def generatesuccessors(board): ##this returns a list of PLACE ACTIONS
     pass
 
@@ -14,13 +23,13 @@ def estimate_cost_totarget(board):
 def goaltest(board, solution): ###Not sure what goal test should be: check the column and the row i assume. but how do u keep track of where ur peices are?
     pass
 
-def calculaute_f(board):
+def calculate_f(board):
     pass
 
 def updateboard(board):
     pass #not sure if i need this?
 
-def search(board: dict[Coord, PlayerColor], target: Coord) -> list[PlaceAction] | None:
+def searchattempt1(board: dict[Coord, PlayerColor], target: Coord) -> list[PlaceAction] | None:
     
     solution = [] #Initialse solution list
     pq = PriorityQueue() #initialize priority queue
@@ -40,13 +49,30 @@ def search(board: dict[Coord, PlayerColor], target: Coord) -> list[PlaceAction] 
 
 
 
-
-   
     return [
         PlaceAction(Coord(2, 5), Coord(2, 6), Coord(3, 6), Coord(3, 7)),
         PlaceAction(Coord(1, 8), Coord(2, 8), Coord(3, 8), Coord(4, 8)),
         PlaceAction(Coord(5, 8), Coord(6, 8), Coord(7, 8), Coord(8, 8)),
     ]
+
+def search(board: dict[Coord, PlayerColor], target: Coord) -> list[PlaceAction] | None:
+    
+    solution = [] #Initialse solution list
+    pq = PriorityQueue() #initialize priority queue
+    initial_state = board 
+    
+
+    pq.put(0, initial_state)  #putting the initial state in the queue, not sure if i sould use 0?
+    
+    while not pq.empty():
+        nextmove = pq.dequeue()
+        if (nextmove == target):
+                return solution
+        else:
+            solution.append(nextmove)
+            for i in generatesuccessors(best_state): ##keep in mind that the type of i is placeaction!!!!!!!!!
+               value =calculate_f(i)
+               pq.enqueue(i,value)
 
 
 
@@ -65,3 +91,5 @@ def search(board: dict[Coord, PlayerColor], target: Coord) -> list[PlaceAction] 
         A list of "place actions" as PlaceAction instances, or `None` if no
         solution is possible.
     """
+
+    
