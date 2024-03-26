9f618fb074823946fc27488d19b95a5a3fffe5ec
victoriahason <157922655+victoriahason@users.noreply.github.com>
2024-03-26 19:57:09 +1100

    Add files via upload

---
+++ part_a/search/program.py
@@ -0,0 +1,34 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part A: Single Player Tetress
+
+from .core import PlayerColor, Coord, PlaceAction
+from .utils import render_board
+
+
+def search(
+    board: dict[Coord, PlayerColor], 
+    target: Coord
+) -> list[PlaceAction] | None:
+    """
+    This is the entry point for your submission. You should modify this
+    function to solve the search problem discussed in the Part A specification.
+    See `core.py` for information on the types being used here.
+
+    Parameters:
+        `board`: a dictionary representing the initial board state, mapping
+            coordinates to "player colours". The keys are `Coord` instances,
+            and the values are `PlayerColor` instances.  
+        `target`: the target BLUE coordinate to remove from the board.
+    
+    Returns:
+        A list of "place actions" as PlaceAction instances, or `None` if no
+        solution is possible.
+    """
+
+   
+    return [
+        PlaceAction(Coord(2, 5), Coord(2, 6), Coord(3, 6), Coord(3, 7)),
+        PlaceAction(Coord(1, 8), Coord(2, 8), Coord(3, 8), Coord(4, 8)),
+        PlaceAction(Coord(5, 8), Coord(6, 8), Coord(7, 8), Coord(8, 8)),
+    ]
+
