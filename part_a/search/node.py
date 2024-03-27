f135033c83339aeb08addb993c889dd6cb69e586
victoriahason <157922655+victoriahason@users.noreply.github.com>
2024-03-27 12:37:43 +0000

    made a node class??????? idk if its needed but yeah

---
+++ part_a/search/node.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, coord):
+        self.coord = coord
+        self.parent = None
+        self.g = 0
+        self.h = 0
+
+    def set_g(g):
+        self.g =g
+    
+    def set_h(h):
+        self.h = h
+
+    def set_parent(node):
+        self.parent = node
